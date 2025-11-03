230a792f27db7aa08793002975f4cb8b813bd584
김현빈 <bin031001@gmail.com>
2025-11-03 22:57:18 +0900

    Add basic directory structure and initial index layout

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let ipipBtn = document.querySelector("#btn-ipip");
+let icarBtn = document.querySelector("#btn-icar");
+
+ipipBtn.addEventListener("click", () => {
+  window.location.href = "tests/ipip.html";
+});
+
+icarBtn.addEventListener("click", () => {
+  window.location.href = "tests/icar.html";
+});
